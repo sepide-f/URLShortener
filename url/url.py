b476547574191536bf5fe9d80bcd688b20166d47
sepide <135548271+sepide-f@users.noreply.github.com>
2024-10-26 11:44:16 +0330

    Update url.py

--- url/url.py
+++ url/url.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from .views import URLShortenerView
+
+urlpatterns = [
+    path("shorten/", URLShortenerView.as_view(), name='shortener'),
+    path("shorten/<str:short_code>/", URLShortenerView.as_view(), name='shortener'),
+    path("put/shorten/<str:short_code>/", URLShortenerView.as_view(), name="shortener"),
+    path("delete/shorten/<str:short_code>/", URLShortenerView.as_view(), name="shortener"),
+]
 
