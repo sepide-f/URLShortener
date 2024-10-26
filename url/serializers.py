d0b5ea5e86cf8d713501fb5771bcb6e68e8bd8a5
sepide <135548271+sepide-f@users.noreply.github.com>
2024-10-26 11:43:49 +0330

    Update serializers.py

--- url/serializers.py
+++ url/serializers.py
@@ -1,1 +1,11 @@
+from rest_framework import serializers
+from .models import URLShortener
+
+
+class URLShortenerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = URLShortener
+        fields = ['id', 'url', 'short_code', 'created_at', 'updated_at']
+
+    url = serializers.URLField()
 
