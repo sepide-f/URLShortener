6b1354a569d750c0a6a62d0144f015d6a5d536cd
sepide <135548271+sepide-f@users.noreply.github.com>
2024-10-26 11:43:35 +0330

    Update models.py

--- url/models.py
+++ url/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+from django.utils import timezone
 
+
+class URLShortener(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=500)
+    short_code = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"
